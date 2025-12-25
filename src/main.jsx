861b5c2e730b8dd4b32319ab6af94b90a7aff54f
aaljo2 <aaljo2@naver.com>
2025-12-25 09:53:27 +0900

    tilewind css

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
+import "./index.css";  // ⭐ 이 줄 추가
 import App from "./App";
 import store from "./store";
 
