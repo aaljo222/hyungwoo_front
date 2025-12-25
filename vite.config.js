4958fb0dde5deeb0958e67b77f4d3e079387904d
aaljo2 <aaljo2@naver.com>
2025-12-25 10:16:01 +0900

    tilewind css

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: "automatic", // ⭐ 이게 핵심
+      jsxRuntime: "automatic",
     }),
   ],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  base: "/",
 });
